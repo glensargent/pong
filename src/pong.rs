b39b375fd693df1b62c7f887e4f8fb8d7869ddcc
Glen Sargent <glensarg@hotmail.co.uk>
2020-09-21 21:53:07 +0100

    add paddle entities

--- src/pong.rs
+++ src/pong.rs
@@ -9,11 +9,16 @@ use amethyst::{
 // dimensions of playable area
 pub const ARENA_HEIGHT: f32 = 100.0;
 pub const ARENA_WIDTH: f32 = 100.0;
+// paddle dimensions
+pub const PADDLE_HEIGHT: f32 = 16.0;
+pub const PADDLE_WIDTH: f32 = 4.0;
 
 pub struct Pong;
 impl SimpleState for Pong{
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
         let world = data.world;
+        world.register::<Paddle>();
+        initialise_paddles(world);
         initialise_camera(world);
     }
 }
@@ -28,4 +33,54 @@ fn initialise_camera(world: &mut World) {
         .with(Camera::standard_2d(ARENA_WIDTH, ARENA_HEIGHT))
         .with(transform)
         .build();
+}
+
+#[derive(PartialEq, Eq)]
+pub enum Side {
+    Left,
+    Right,
+}
+
+pub struct Paddle {
+    pub side: Side,
+    pub width: f32,
+    pub height: f32,
+}
+
+impl Paddle {
+    fn new(side: Side) -> Paddle {
+        Paddle {
+            side,
+            width: PADDLE_WIDTH,
+            height: PADDLE_HEIGHT,
+        }
+    }
+}
+
+impl Component for Paddle {
+    type Storage = DenseVecStorage<Self>;
+}
+
+// initialise 2 paddles, one on the left and one on the right
+fn initialise_paddles(world: &mut World) {
+    let mut left_transform = Transform::default();
+    let mut right_transform = Transform::default();
+
+    // position paddles
+    let y = ARENA_HEIGHT / 2.0; // centre it on the Y
+    left_transform.set_translation_xyz(PADDLE_WIDTH * 0.5, y, 0.0);
+    right_transform.set_translation_xyz(ARENA_WIDTH - PADDLE_WIDTH * 0.5, y, 0.0);
+
+    // create left plank entity
+    world.create_entity()
+        .with(Paddle::new(Side::Left))
+        .with(left_transform)
+        .build();
+
+    // create right plank entity
+    world.create_entity()
+        .with(Paddle::new(Side::Right))
+        .with(right_transform)
+        .build();
+
 }
